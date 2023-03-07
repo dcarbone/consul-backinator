f28207f4579f46f3b5071d3c2766cc0f47dc13ed
boagg <agbosteven@gmail.com>
2023-03-07 12:49:14 +0100

    Update models.go
    Need to restore Policy before

--- common/models.go
+++ common/models.go
@@ -3,7 +3,7 @@ package common
 import "github.com/hashicorp/consul/api"
 
 type BackupACLData struct {
-	Roles    []*api.ACLRole
 	Policies []*api.ACLPolicy
+	Roles    []*api.ACLRole
 	Tokens   []*api.ACLToken
 }
